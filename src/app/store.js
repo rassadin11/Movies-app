a90c7d2fbfd8e550ea6bc2cad1cc792d2512162f
Artyom Rassadin <58170052+rassadin11@users.noreply.github.com>
2021-12-14 16:38:55 +0300

    Fix something

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
-<<<<<<< HEAD
-import counterSlice from '../redux/reducers/counterSlice';
-=======
 import catalogSlice from '../redux/reducers/catalogSlice';
 import movieSlice from '../redux/reducers/movieSlice';
->>>>>>> a265d12c83cad46ae665f9fc784bd7b1a6861391
 
 export const store = configureStore({
   reducer: {
